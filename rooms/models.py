532cfd1b7bf779151763320909c99bb139e7b081
Jade E Lopez <38263897+jadeli1720@users.noreply.github.com>
2020-02-04 20:25:23 -0700

    Defined Rooms class in  models.py for rooms

--- rooms/models.py
+++ rooms/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Rooms(models.Model):
+    room_id = models.IntegerField(primary_key=True, unique=True, editable=False)
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+    coordinates = models.CharField(max_length=200)
+    # May need adjustments for array fields. blank=True allows for empty fields
+    players = ArrayField(models.CharField(), blank=True)
+    items = ArrayField(models.CharField(), blank=True)
+    exits = ArrayField(models.CharField(), blank=True)
+    cooldown = models.IntegerField(editable=False, blank=False)
+    errors = ArrayField(models.CharField(), blank=True)
+    messages = ArrayField(models.CharField(), blank=True)
