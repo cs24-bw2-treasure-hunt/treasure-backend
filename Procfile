d2ade83e592b00815805c31c9c370ecf5ac40353
Jade E Lopez <38263897+jadeli1720@users.noreply.github.com>
2020-02-05 00:13:00 -0700

    Debugging heroku

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn treasure_database.wsgi —-log-file -
+web: gunicorn treasure_database.wsgi:application —-log-file -
--- treasure_database/settings.py
+++ treasure_database/settings.py
@@ -14,6 +14,7 @@ import os
 import django_heroku
 
 from decouple import config
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -76,12 +77,21 @@ WSGI_APPLICATION = 'treasure_database.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
     }
 }
+
+DATABASES['default'] = dj_database_url.config(default=config('DATABASE_URL'))
+# DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 
 # Password validation
